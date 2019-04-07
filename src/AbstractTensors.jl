091f5792663f8ab0112ffbdaa0338baa14d8cb61
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-07 18:54:48 +0000

    Snapshot of branch master taken on 2019-04-07T14:54:47.338-04:00 from "https://github.com/UnofficialJuliaMirror/AbstractTensors.jl-a8e43f4a-99b7-5565-8bf1-0165161caaea"

---
+++ src/AbstractTensors.jl
@@ -0,0 +1,57 @@
+
+#   This file is part of AbstractTensors.jl. It is licensed under the GPL license
+#   Grassmann Copyright (C) 2019 Michael Reed
+
+module AbstractTensors
+
+# universal root Tensor type
+
+abstract type TensorAlgebra{V} end
+
+# V, VectorSpace produced by DirectSum
+
+import DirectSum: vectorspace, value
+import LinearAlgebra: dot, cross, UniformScaling
+
+# parameters accessible from anywhere
+
+Base.@pure vectorspace(::T) where T<:TensorAlgebra{V} where V = V
+
+# universal vector space interopability
+
+@inline interop(op::Function,a::A,b::B) where {A<:TensorAlgebra{V},B<:TensorAlgebra{V}} where V = op(a,b)
+
+# ^^ identity ^^ | vv union vv #
+
+@inline function interop(op::Function,a::A,b::B) where {A<:TensorAlgebra{V},B<:TensorAlgebra{W}} where {V,W}
+    VW = V ∪ W
+    return op(VW(a),VW(b))
+end
+
+# abstract tensor form evaluation
+
+@inline interform(a::A,b::B) where {A<:TensorAlgebra{V},B<:TensorAlgebra{V}} where V = a(b)
+@inline function interform(a::A,b::B) where {A<:TensorAlgebra{V},B<:TensorAlgebra{W}} where {V,W}
+    VW = V ∪ W
+    return VW(a)(VW(b))
+end
+
+# extended compatibility interface
+
+export interop, TensorAlgebra, interform, ⊗
+
+# some shared presets
+
+for op ∈ (:(Base.:+),:(Base.:-),:(Base.:*),:⊗,:dot,:cross,:(Base.:(==)))
+    @eval begin
+        @inline $op(a::A,b::B) where {A<:TensorAlgebra,B<:TensorAlgebra} = interop($op,a,b)
+        @inline $op(a::A,b::UniformScaling) where A<:TensorAlgebra{V} where V = $op(a,V(b))
+        @inline $op(a::UniformScaling,b::B) where B<:TensorAlgebra{V} where V = $op(V(a),b)
+    end
+end
+
+# absolute value norm
+
+@inline Base.abs(t::T) where T<:TensorAlgebra = sqrt(abs(value(dot(t,t))))
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,32 @@
+using AbstractTensors
+using Test, DirectSum, LinearAlgebra
+import DirectSum: VectorSpace
+
+# example data
+struct SpecialTensor{V} <: TensorAlgebra{V} end
+
+## tensor operation (trivial test)
+op(s::SpecialTensor{V},::SpecialTensor{V}) where V = s
+op(a::TensorAlgebra{V},b::TensorAlgebra{W}) where {V,W} = interop(op,a,b)
+(W::Signature)(s::SpecialTensor{V}) where V = SpecialTensor{W}()
+@test vectorspace(op(SpecialTensor{ℝ}(),SpecialTensor{ℝ'}())) == ℝ⊕ℝ'
+@test vectorspace(interop(op,SpecialTensor{ℝ}(),SpecialTensor{ℝ'}())) == ℝ⊕ℝ'
+Base.:+(s::SpecialTensor{V},::SpecialTensor{V}) where V = s
+@test vectorspace(+(SpecialTensor{ℝ}(),SpecialTensor{ℝ'}())) == ℝ⊕ℝ'
+@test vectorspace(interop(+,SpecialTensor{ℝ}(),SpecialTensor{ℝ'}())) == ℝ⊕ℝ'
+
+
+## tensor pseudoscalar (trivial test)
+op(a::TensorAlgebra{V},b::UniformScaling) where V = op(a,V(b))
+op(a::UniformScaling,b::TensorAlgebra{V}) where V = op(V(a),b)
+(W::Signature)(s::UniformScaling) where V = SpecialTensor{W}()
+@test vectorspace(op(SpecialTensor{ℝ}(),LinearAlgebra.I)) == ℝ
+@test vectorspace(op(LinearAlgebra.I,SpecialTensor{ℝ}())) == ℝ
+@test vectorspace(+(SpecialTensor{ℝ}(),LinearAlgebra.I)) == ℝ
+@test vectorspace(+(LinearAlgebra.I,SpecialTensor{ℝ}())) == ℝ
+
+## tensor evaluation (trivial test)
+(a::SpecialTensor{V})(b::SpecialTensor{V}) where V = a
+(a::SpecialTensor{W})(b::SpecialTensor{V}) where {V,W} = interform(a,b)
+@test vectorspace(SpecialTensor{ℝ'}()(SpecialTensor{ℝ}())) == ℝ⊕ℝ'
+@test vectorspace(interform(SpecialTensor{ℝ'}(),SpecialTensor{ℝ}())) == ℝ⊕ℝ'
